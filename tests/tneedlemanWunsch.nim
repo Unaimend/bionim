7af71e232c6adcf68bc5854c3b32c1a54913629f
td <td@td-debian>
2021-03-03 23:48:30 +0100

    Added one test

--- tests/tneedlemanWunsch.nim
+++ tests/tneedlemanWunsch.nim
@@ -1,18 +1,18 @@
 import std/unittest
 import bionim/needlemanWunsch
-
+import bionim/utils
 
 proc needlemanWunschMain() =
   suite "Main test":
     setup:
-      let seq1 = "WHAT"
-      let seq2 = "WHY"
+      var seq1 = "WHAT"
+      var seq2 = "WHY"
 
       let indel_penal: int8 = -1
-      let gap_penal: int8 = -1
+      let mismatch: int8 = -1
       let match: int8 = 1
 
-      let grid = needlemanWunsch(seq1, seq2, gap_penal, match, indel_penal)
+      var grid = needlemanWunsch(seq1, seq2, indel_penal, match, mismatch)
     test "Build grid":
       var test: Matrix 
       new(test)
@@ -21,12 +21,26 @@ proc needlemanWunschMain() =
       test[1] = @[-1,1,0,-1, -2]
       test[2] = @[-2,0,2,1, 0]
       test[3] = @[-3,-1,1,1, 0]
-      printGrid(test, "WHAT", "WHY")
       check(grid[] == test[])
-    test "Align":
-      let alignment = calculateAlignment(grid, seq1, seq2, gap_penal, match, indel_penal)
+    
+
+    test "Align normale":
+      let alignment = calculateAlignment(grid, seq1, seq2, indel_penal, match, mismatch)
       check(alignment[0] == "WHAT")
       check(alignment[1] == "WH-Y")
+
+    test "Build grid first single":
+      seq1 = "W"
+      seq2 = "WHY"
+      grid = needlemanWunsch(seq1, seq2, indel_penal, match, mismatch)
+      var test: Matrix 
+      new(test)
+      test[] = newSeq[seq[int]](seq2.len+1)
+      test[0] = @[0, -1]
+      test[1] = @[-1, 1]
+      test[2] = @[-2, 0]
+      test[3] = @[-3, -1]
+      check(grid[] == test[])
 
 
 needlemanWunschMain()
